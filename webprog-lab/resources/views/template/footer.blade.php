c927a54a1fcbe6c3b91dbec3a93fb689c0bd1af9
Michael <michaelgunawan55@gmail.com>
2023-01-03 22:07:00 +0700

    Update category

---
+++ webprog-lab/resources/views/template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-light text-center text-lg-start mt-2">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(5, 5, 5, 0.2);">
+      © 2022 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">Giant Book Supplier</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
